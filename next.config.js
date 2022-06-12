fbb5018b1974fe379327cd290a0e4d86c96267c9
Boonsuen Oh <boonsuenoh@gmail.com>
2022-06-12 17:26:52 +0800

    Add rewrites

--- next.config.js
+++ next.config.js
@@ -15,6 +15,30 @@
         source: '/process-scheduling-solver/:slug*',
         destination: 'https://process-scheduling-solver.vercel.app/process-scheduling-solver/:slug*',
       },
+      {
+        source: '/hodler',
+        destination: 'https://hodler.netlify.app',
+      },
+      {
+        source: '/hodler/:slug*',
+        destination: 'https://hodler.netlify.app/:slug*',
+      },
+      {
+        source: '/tictactoe',
+        destination: 'https://tictactoe-bs.netlify.app',
+      },
+      {
+        source: '/tictactoe/:slug*',
+        destination: 'https://tictactoe-bs.netlify.app/:slug*',
+      },
+      {
+        source: '/hodler-serverful',
+        destination: 'https://hodler-app.herokuapp.com',
+      },
+      {
+        source: '/hodler-serverful/:slug*',
+        destination: 'https://hodler-app.herokuapp.com/:slug*',
+      },
     ]
   },
 };
