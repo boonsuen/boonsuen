d7342c223be12d579ce93b883dc016e44421eef3
Boonsuen Oh <boonsuenoh@gmail.com>
2022-06-12 16:50:31 +0800

    Rewrites

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@
   compiler: {
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/process-scheduling-solver',
+        destination: 'https://process-scheduling-solver.vercel.app/process-scheduling-solver',
+      },
+      {
+        source: '/process-scheduling-solver/:slug*',
+        destination: 'https://process-scheduling-solver.vercel.app/process-scheduling-solver/:slug*',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
